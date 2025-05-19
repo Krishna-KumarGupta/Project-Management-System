ef0a3e85603dc291b59202118e1574e96b2022a7
Krishna Kumar Gupta <krishnagupta09062005@gmail.com>
2025-05-19 12:08:01 +0530

    Refactor application.properties to use environment variables for deployment

--- React/project-management-react/vite.config.js
+++ React/project-management-react/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Project-Management-System/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
