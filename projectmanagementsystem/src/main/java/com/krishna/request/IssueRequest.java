366fffa7d2274414517400690bd686c0ccf8092c
Krishna-KumarGupta <krishnagupta09062005@gmail.com>
2026-06-07 23:49:11 +0530

    Update project configuration and dependencies
    - Changed Vite base path in vite.config.js from '/Project-Management-System/' to '/' for improved routing.
    - Updated Java version in pom.xml from 17 to 25 and adjusted Lombok version from 1.18.30 to 1.18.42.
    - Modified Maven compiler plugin configuration to use Java 24 release instead of specifying source and target versions.
    - Cleaned up IssueRequest.java by removing unnecessary comment on projectId field.

--- projectmanagementsystem/src/main/java/com/krishna/request/IssueRequest.java
+++ projectmanagementsystem/src/main/java/com/krishna/request/IssueRequest.java
@@ -10,7 +10,7 @@ public class IssueRequest {
     private String title;
     private String description;
     private String status;
-    private Long projectId; //
+    private Long projectId;
     private String priority;
     private LocalDate dueDate ;
 }
